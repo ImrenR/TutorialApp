21f7ee1740d48cd26ebfac99216335b09f750b81
ImrenR <imrenrahbay@gmail.com>
2025-09-07 14:56:14 +0100

    Data called, and checked

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import AddTutorial from "../components/AddTutorial"
 import TutorialList from "../components/TutorialList"
+import axios from "axios"
+
 
 
 const Home = () => {
+
+const getTutorial = async()=>{
+  try {
+    const result = await axios.get(import.meta.env.VITE_APP_URL)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+getTutorial();
+
+
   return <div>
 <AddTutorial/>
 <TutorialList/>
