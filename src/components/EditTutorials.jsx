11bfce13df6553f4b7f2d15b5a24a97783a19550
ImrenR <imrenrahbay@gmail.com>
2025-09-07 17:16:07 +0100

    modal editing

--- src/components/EditTutorials.jsx
+++ src/components/EditTutorials.jsx
@@ -1,69 +1,88 @@
+import { useState } from "react";
+
 const EditTutorials = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <>
-        {/* Button trigger modal */}
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          Edit Tutorials
-        </button>
-        {/* Modal */}
-        <div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex={-1}
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-body">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <h1 className="text-danger">Edit Your Tutorial </h1>
-                <label
-                  htmlFor="exampleFormControlInput1"
-                  className="form-label"
-                >
-                  Title
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  placeholder="Enter your title"
-                  required
-                  value={title}
-                />
-              </div>
-              <div className="mb-3">
-                <label
-                  htmlFor="exampleFormControlTextarea1"
-                  className="form-label"
-                >
-                  Description
-                </label>
-                <textarea
-                  className="form-control"
-                  id="desc"
-                  rows={1}
-                  placeholder="Enter your Description"
-                  value={description}
-                  required
-                />
-              </div>
-            </form>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-primary">
-              Submit
-            </button>
+    <>
+      {/* Button trigger modal */}
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modal title
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    placeholder="Enter your title"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlTextarea1"
+                    className="form-label"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="desc"
+                    rows={1}
+                    placeholder="Enter your Description"
+                    value={description}
+                    required
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <button type="submit" class="btn btn-danger">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </>
-    </div>
+      </div>
+    </>
   );
 };
 
--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,22 +1,18 @@
 import { MdDeleteOutline } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import axios from "axios";
+import EditTutorials from "./EditTutorials";
 
-
-
-const TutorialList = ({tutorials, getTutorials}) => {
-
-
-const deleteTutorials = async(id)=> {
- try {
+const TutorialList = ({ tutorials, getTutorials }) => {
+  const deleteTutorials = async (id) => {
+    try {
       await axios.delete(`${import.meta.env.VITE_APP_URL}${id}/`);
     } catch (error) {
       console.log(error);
     } finally {
       getTutorials();
     }
-}
-
+  };
 
   return (
     <div>
@@ -36,29 +32,31 @@ const deleteTutorials = async(id)=> {
           </tr>
         </thead>
         <tbody>
-          {tutorials.map((item)=> {
-            const {id, title, description} = item
+          {tutorials.map((item) => {
+            const { id, title, description } = item;
             return (
-     <tr className="bg-light">
-            <th scope="row">{id}</th>
-            <td>{title}</td>
-            <td>{description}</td>
-            <td className="text-center text-nowrap">
-              <button type="submit">
-                <CiEdit size={22} className="me-2 btn-warning" />
-              </button>
-              <button 
-              type="submit"
-              onClick={()=>deleteTutorials(id)}>
-                <MdDeleteOutline size={22} className="text-danger" />
-              </button>
-            </td>
-          </tr>
-            )
+              <tr className="bg-light">
+                <th scope="row">{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center text-nowrap">
+                  <button
+                    type="submit"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                  >
+                    <CiEdit size={22} className="me-2 btn-warning" />
+                  </button>
+                  <button type="submit" onClick={() => deleteTutorials(id)}>
+                    <MdDeleteOutline size={22} className="text-danger" />
+                  </button>
+                </td>
+              </tr>
+            );
           })}
-     
         </tbody>
       </table>
+      <EditTutorials/>
     </div>
   );
 };
